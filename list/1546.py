cc17039c7f63efc92c5ffb37b35eda0c9be7e259
hyetjs <cherryade98@gmail.com>
2023-06-18 12:22:12 +0900

    :memo:update

--- list/1546.py
+++ list/1546.py
@@ -1,12 +1,12 @@
 a = int(input())
 l = [0] * a
-b = [0] * len(l)
+b = [0] * a
 c = 0
 k = list(map(int,input().split()))
-for i in range(len(l)):
+for i in range(a):
     l[i] = k[i]
     
-for x in range(len(l)):
+for x in range(a):
     b[x] = l[x] / max(l) * 100
 for y in range(len(b)):
     c += b[y]
